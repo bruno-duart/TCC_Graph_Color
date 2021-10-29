8359bff7e1eef7a30f8e63a642d4c5194cfdcbca
bruno-duart <53445564+bruno-duart@users.noreply.github.com>
2021-10-29 20:23:34 -0300

    Implementaçao da Busca tabu

--- forward_list.h
+++ forward_list.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int type_t;
-
 typedef struct _node_t{
     struct _node_t *next;
-    type_t value;
+    int count_iter;
+    int index_i;
+    int undo_color;
 }node_t;
 
 typedef struct _list_t{
@@ -13,19 +13,20 @@ typedef struct _list_t{
     int size_list;
 }list_t;
 
-node_t *new_node(type_t value);
+node_t *new_node(int index_i, int undo_color, int T_iter);
 list_t *new_list();
 void free_list(list_t *l);
 
 node_t *list_at(list_t *l, int position);
-void list_set(list_t *l, int position, type_t value);
-type_t list_size(list_t *l);
+void list_set(list_t *l, int position, int index_i, int undo_color);
+int list_search_by_value(list_t *l, int index_i, int undo_color);
+int list_size(list_t *l);
 void print_list(list_t *l);
 
-void list_insert(list_t *l, type_t value, int position);
-void list_push_front(list_t *l, type_t value);
-void list_push_back(list_t *l, type_t value);
+void list_insert(list_t *l, int index_i, int undo_color, int T_iter, int position);
+void list_push_front(list_t *l, int index_i, int undo_color, int T_iter);
+void list_push_back(list_t *l, int index_i, int undo_color, int T_iter);
 
-type_t list_erase(list_t *l, int position);
-type_t list_pop_front(list_t *l);
-type_t list_pop_back(list_t *l);
+int * list_erase(list_t *l, int position);
+int * list_pop_front(list_t *l);
+int * list_pop_back(list_t *l);
